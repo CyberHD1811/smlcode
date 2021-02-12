e0ef57a46f61258b94ae60596cd1d9e7bf00959f
CyberHD1811 <robin.timmermann@gmx.de>
2021-02-12 12:02:58 +0100

    Version 1.0.0
    First commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# smlcode
+
+---
+
+This is a small package to create a custom code.
+
+<br>
+
+```js
+import { smlCode } from 'smlcode';
+
+console.log(smlCode(5, 8));
+//Expected Output (ONLY an example): a4kf9rh7-c58g94dk-p5kl4aso-bz6pcdlt-1qu5dk2c
+```
+
+---
+
+## Documentation:
+
+- #### smlCode(section, innersection):
+    - ##### section: A number >= 1
+    - ##### innersection: A number >= 1
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+const smlCode = (sections: number = 1, innerSections: number = 1) => {
+  if (!sections)
+    throw new Error(
+      `You haven't defined how many sections you want to use. Please specify a section (a number higher than or equal to 1.`,
+    );
+  else if (typeof sections !== 'number') {
+    throw new TypeError(
+      `Your input value " ${sections} " is a ${typeof sections} and not a number, please try a number higher than or equal to 1 as input.`,
+    );
+  }
+  if (!innerSections)
+    throw new Error(
+      `You haven't defined how many characters you want to use in one section. Please specify a number between 1 and 10 for it.`,
+    );
+  else if (typeof innerSections !== 'number') {
+    throw new TypeError(
+      `Your input value " ${innerSections} " for the length of one section is not a number, please try a number between 1 and 10 as input.`,
+    );
+  }
+
+  const Sections = Math.round(sections);
+  const InnerSections = Math.round(innerSections);
+
+  let CString: string = '';
+  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < sections; i++) {
+    if (i === 0) {
+      for (let j = 0; j < InnerSections; j++) {
+        CString += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+    } else {
+      CString += '-';
+      for (let k = 0; k < InnerSections; k++) {
+        CString += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+    }
+  }
+  return CString;
+};
+
+export { smlCode };
