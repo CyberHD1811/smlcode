a999deb35521517e776c1698cf6b39694be7326c
CyberHD1811 <robin.timmermann@gmx.de>
2021-02-14 13:38:32 +0100

    Version 1.2.0
    Added feature to use a custom charset.

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ This is a small package to create a custom code.
 ```js
 import { smlCode } from 'smlcode';
 
-console.log(smlCode(5, 8));
+const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+console.log(smlCode(5, 8, chars));
 //Expected Output (ONLY an example): a4kf9rh7-c58g94dk-p5kl4aso-bz6pcdlt-1qu5dk2c
 ```
 
@@ -24,6 +26,7 @@ You will als get an Error if one one the Values is not a number.
 
 ## Documentation:
 
-- #### smlCode(section, innersection):
+- #### smlCode(section, innersection, ):
     - ##### section: A number >= 1
     - ##### innersection: A number >= 1
+    - ##### characters: A custom string with a minimum of 1 character (It's recommended to use at least 10 different characters)
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
-const smlCode = (sections: number = 1, innerSections: number = 1) => {
+const smlCode = (sections: number, innerSections: number, characters: string = 'abcdefghijklmnopqrstuvwxyz0123456789') => {
     if (!sections) throw new Error(`You haven't defined how many sections you want to use. Please specify a section (a number higher than or equal to 1.`);
-    else if (typeof sections !== 'number') throw new TypeError(`Your input value " ${sections} " is a ${typeof sections} and not a number, please try a number higher than or equal to 1 as input.`);
+    else if (typeof sections !== 'number') throw new TypeError(`Your input value " ${sections} " is a(n) ${typeof sections} and not a number, please try a number higher than or equal to 1 as input.`);
     
     if (!innerSections) throw new Error(`You haven't defined how many characters you want to use in one section. Please specify a number between 1 and 10 for it.`);
     else if (typeof innerSections !== 'number') throw new TypeError(`Your input value " ${innerSections} " for the length of one section is not a number, please try a number between 1 and 10 as input.`);
+
+    if (typeof characters !== 'string') throw new TypeError(`Your input value " ${characters} " for the characters is a(n) ${typeof characters} and not a string.`);
+    else if (!characters) throw new Error(`You haven't defined characters you want to use. Please specify at least 1 character or number (as string!). (It's also recommended to use a minimum of 10 different characters)`);
 
     const Sections = Math.round(sections);
     const InnerSections = Math.round(innerSections);
 
     let CString: string = '';
-    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
     for (let i = 0; i < sections; i++) {
         if (i === 0) {
             for (let j = 0; j < InnerSections; j++) {
-                CString += chars.charAt(Math.floor(Math.random() * chars.length));
+                CString += characters.charAt(Math.floor(Math.random() * characters.length));
             }
         } else {
             CString += '-';
             for (let k = 0; k < InnerSections; k++) {
-                CString += chars.charAt(Math.floor(Math.random() * chars.length));
+                CString += characters.charAt(Math.floor(Math.random() * characters.length));
             }
         }
     }
